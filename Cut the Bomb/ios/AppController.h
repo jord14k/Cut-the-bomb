b59f8e9202527e002d5d194a172779970720fdec
jordan Cortet <jord@MacBook-Pro-de-jordan.local>
2013-07-10 22:51:18 +0200

    Initial Commit

---
+++ Cut the Bomb/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Cut_the_BombAppController.h
+//  Cut the Bomb
+//
+//  Created by jordan Cortet on 10/07/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Cut the Bomb/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Cut_the_BombAppController.h
+//  Cut the Bomb
+//
+//  Created by jordan Cortet on 10/07/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
